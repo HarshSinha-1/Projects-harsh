8a9a61534ff012f792b1defdabaddbddf6b1b7a1
HarshSinha-1 <harshsinha969@gmail.com>
2024-10-30 08:48:25 +0530

    Delete configs directory

--- configs/db.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    DB_NAME : "ecomm_db",
-    DB_URL : "mongodb://localhost/ecomm_db"  //0.0.0.0
-}
--- configs/server.config.js
+++
@@ -1,6 +0,0 @@
-/**
- * This file will contain the server configurations
- */
-module.exports = {
-    PORT : 8888
-}
